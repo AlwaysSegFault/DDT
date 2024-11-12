2aba55cb39a05ab59780916c24f42f9953bedc18
Асхабов Дени <askhabovdenidev@gmail.com>
2024-11-12 18:30:36 +0300

    Create client.c

---
+++ client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *message = "Hello from client";
+    char buffer[1024] = {0};
+
+    // Создаем сокет
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        printf("\nSocket creation error\n");
+        return -1;
+    }
+
+    // Настройка адреса сервера
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Преобразование IP-адреса
+    if (inet_pton(AF_INET, "192.168.1.2", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address or Address not supported\n");
+        return -1;
+    }
+
+    // Подключение к серверу
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection failed\n");
+        return -1;
+    }
+
+    // Отправка сообщения серверу
+    send(sock, message, strlen(message), 0);
+    printf("Message sent\n");
+
+    // Чтение ответа от сервера
+    read(sock, buffer, 1024);
+    printf("Received: %s\n", buffer);
+
+    close(sock);
+    return 0;
+}
