029c1eff50572a061d713c698787775ea788e503
Zelim <justudent09@mail.ru>
2024-11-15 15:25:29 +0300

    создание мессенджера

--- client.c
+++ client.c
@@ -3,19 +3,34 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 8080
 
+void *receive_messages(void *socket_desc) {
+    int sock = *(int *)socket_desc;
+    char buffer[1024];
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_read = read(sock, buffer, sizeof(buffer));
+        if (bytes_read <= 0) {
+            printf("Disconnected from server\n");
+            break;
+        }
+        printf("Server: %s\n", buffer);
+    }
+    pthread_exit(NULL);
+}
+
 int main() {
-    int sock = 0;
+    int sock;
     struct sockaddr_in serv_addr;
-    char *message = "000";
     char buffer[1024] = {0};
 
     // Создаем сокет
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        printf("\nSocket creation error\n");
+        perror("Socket creation error");
         return -1;
     }
 
@@ -24,24 +39,31 @@ int main() {
     serv_addr.sin_port = htons(PORT);
 
     // Преобразование IP-адреса
-    if (inet_pton(AF_INET, "172.28.117.177", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address or Address not supported\n");
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address/ Address not supported\n");
         return -1;
     }
 
     // Подключение к серверу
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection failed\n");
+        perror("Connection failed");
         return -1;
     }
 
-    // Отправка сообщения серверу
-    send(sock, message, strlen(message), 0);
-    printf("Message sent\n");
+    printf("Connected to server\n");
 
-    // Чтение ответа от сервера
-    read(sock, buffer, 1024);
-    printf("Received: %s\n", buffer);
+    pthread_t receive_thread;
+    if (pthread_create(&receive_thread, NULL, receive_messages, (void *)&sock) != 0) {
+        perror("Thread creation failed");
+        return -1;
+    }
+
+    while (1) {
+        printf("You: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = 0; // Remove trailing newline
+        send(sock, buffer, strlen(buffer), 0);
+    }
 
     close(sock);
     return 0;
