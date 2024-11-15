db72462bdfa1140e4c059134dc83f308b64b6928
Zelim <justudent09@mail.ru>
2024-11-15 15:47:20 +0300

    крестики-нолики

--- server.c
+++ server.c
@@ -7,23 +7,88 @@
 
 #define PORT 8080
 
+char board[3][3];
+int current_turn = 0; // 0 - сервер (X), 1 - клиент (O)
+
+void initialize_board() {
+    int count = 1;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            board[i][j] = '1' + count - 1;
+            count++;
+        }
+    }
+}
+
+void print_board() {
+    printf("\n");
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            printf(" %c ", board[i][j]);
+            if (j < 2) printf("|");
+        }
+        printf("\n");
+        if (i < 2) printf("---|---|---\n");
+    }
+    printf("\n");
+}
+
+int check_winner() {
+    // Проверка строк, столбцов и диагоналей
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) return board[i][0];
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return board[0][i];
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0];
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2];
+    return 0;
+}
+
 void *handle_client(void *socket_desc) {
     int client_socket = *(int *)socket_desc;
     char buffer[1024] = {0};
+
     while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        int bytes_read = read(client_socket, buffer, sizeof(buffer));
-        if (bytes_read <= 0) {
-            printf("Client disconnected\n");
+        print_board();
+
+        // Проверка победителя
+        int winner = check_winner();
+        if (winner) {
+            char winner_message[50];
+            sprintf(winner_message, "Player %c wins!", winner);
+            send(client_socket, winner_message, strlen(winner_message), 0);
+            printf("%s\n", winner_message);
             break;
         }
-        printf("Client: %s\n", buffer);
 
-        printf("You: ");
-        fgets(buffer, sizeof(buffer), stdin);
-        buffer[strcspn(buffer, "\n")] = 0; // Remove trailing newline
-        send(client_socket, buffer, strlen(buffer), 0);
+        if (current_turn == 1) {
+            memset(buffer, 0, sizeof(buffer));
+            read(client_socket, buffer, sizeof(buffer));
+            int move = atoi(buffer) - 1;
+            int row = move / 3, col = move % 3;
+
+            if (board[row][col] != 'X' && board[row][col] != 'O') {
+                board[row][col] = 'O';
+                current_turn = 0;
+            } else {
+                send(client_socket, "Invalid move", strlen("Invalid move"), 0);
+            }
+        } else {
+            printf("Your move (1-9): ");
+            fgets(buffer, sizeof(buffer), stdin);
+            int move = atoi(buffer) - 1;
+            int row = move / 3, col = move % 3;
+
+            if (board[row][col] != 'X' && board[row][col] != 'O') {
+                board[row][col] = 'X';
+                current_turn = 1;
+                send(client_socket, buffer, strlen(buffer), 0);
+            } else {
+                printf("Invalid move. Try again.\n");
+            }
+        }
     }
+
     close(client_socket);
     free(socket_desc);
     pthread_exit(NULL);
@@ -33,6 +98,8 @@ int main() {
     int server_fd, client_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
+
+    initialize_board();
 
     // Создаем сокет
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
