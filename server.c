a0fee3a17953d7872c4c84ca6a9dc6116e71def8
Zelim <justudent09@mail.ru>
2024-11-15 15:24:45 +0300

    создание мессенджера

--- server.c
+++ server.c
@@ -3,20 +3,41 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 8080
 
+void *handle_client(void *socket_desc) {
+    int client_socket = *(int *)socket_desc;
+    char buffer[1024] = {0};
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_read = read(client_socket, buffer, sizeof(buffer));
+        if (bytes_read <= 0) {
+            printf("Client disconnected\n");
+            break;
+        }
+        printf("Client: %s\n", buffer);
+
+        printf("You: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = 0; // Remove trailing newline
+        send(client_socket, buffer, strlen(buffer), 0);
+    }
+    close(client_socket);
+    free(socket_desc);
+    pthread_exit(NULL);
+}
+
 int main() {
-    int server_fd, new_socket;
+    int server_fd, client_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *response = "Hello from server";
 
     // Создаем сокет
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == 0) {
-        perror("socket failed");
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
@@ -27,35 +48,37 @@ int main() {
 
     // Привязываем сокет к порту
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+        perror("Bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
     // Ожидаем подключения
     if (listen(server_fd, 3) < 0) {
-        perror("listen");
+        perror("Listen");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
 
     printf("Server is listening on port %d\n", PORT);
 
-    // Принимаем входящее соединение
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-    if (new_socket < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
+    while (1) {
+        client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (client_socket < 0) {
+            perror("Accept");
+            continue;
+        }
+        printf("New connection established\n");
+
+        pthread_t client_thread;
+        int *new_sock = malloc(sizeof(int));
+        *new_sock = client_socket;
+        if (pthread_create(&client_thread, NULL, handle_client, (void *)new_sock) != 0) {
+            perror("Thread creation failed");
+            free(new_sock);
+        }
     }
 
-    // Чтение данных от клиента
-    read(new_socket, buffer, 1024);
-    printf("Received: %s\n", buffer);
-
-    // Отправка ответа клиенту
-    send(new_socket, response, strlen(response), 0);
-    printf("Response sent\n");
-
-    close(new_socket);
     close(server_fd);
     return 0;
 }
